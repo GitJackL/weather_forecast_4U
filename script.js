b208888282adc617cd39ca62cd1e20d4cc8006d6
Jack <116763735+GitJackL@users.noreply.github.com>
2023-01-27 15:36:28 +0000

    added function that will retrieve the date from the api, unable to test until api key is active

--- script.js
+++ script.js
@@ -6,6 +6,37 @@ searchBar.on("click", function(event) {
 event.preventDefault();
 //Get city user has entered 
 var city = searchBar.val();
+
+console.log(city);
 // Call function to get weather forecast
-getWeatherForcast(city);
+getWeatherForecast(city);
 });
+
+function getWeatherForecast(city) {
+  var apiKey = '82f437452e5dddfbf7e6a3e2035bb00a';
+  var url = 'https://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid='+apiKey;
+  $.ajax({
+    url: url,
+    type: 'GET',
+    dataType: 'json',
+    success: function(data) {
+        // Do something with the data
+        var temp = data.main.temp;
+        var humidity = data.main.humidity;
+        var windSpeed = data.wind.speed;
+        var forecast = data.list;
+        // update the HTML elements
+        $("#temperature").text(temp);
+        $("#humidity").text(humidity);
+        $("#wind-speed").text(windSpeed);
+        // loop through the forecast array and display the forecast
+        for (var i = 0; i < forecast.length; i++) {
+          var day = forecast[i];
+          var date = new Date(day.dt * 1000);
+          var temp = day.main.temp;
+          $("#forecast").append("<li>" + date + " - " + temp + "</li>");
+        }
+      },
+        // parse the data and display the forecast
+  });
+}
