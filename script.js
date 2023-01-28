b66c172bbedb7b49a26262cd2a6cad6c0f93fce0
Jack <116763735+GitJackL@users.noreply.github.com>
2023-01-28 15:09:00 +0000

    Added ajax and function to take data and display it as cards, created div to append cards to

--- script.js
+++ script.js
@@ -1,42 +1,37 @@
 //Get search bar element
 var searchBar = $("#search-button");
 //Attach event listener
-searchBar.on("click", function(event) {
-//Prevent default submit   
-event.preventDefault();
-//Get city user has entered 
-var city = searchBar.val();
+searchBar.on("click", function (event) {
+  //Prevent default submit   
+  event.preventDefault();
+  //Get city user has entered 
+  var city = $("#search-input").val();
 
-console.log(city);
-// Call function to get weather forecast
-getWeatherForecast(city);
+  console.log(city);
+  // Call function to get weather forecast
+  getWeatherForecast(city);
 });
 
 function getWeatherForecast(city) {
   var apiKey = '82f437452e5dddfbf7e6a3e2035bb00a';
-  var url = 'https://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid='+apiKey;
+  var url = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey;
+  
   $.ajax({
     url: url,
     type: 'GET',
     dataType: 'json',
-    success: function(data) {
-        // Do something with the data
-        var temp = data.main.temp;
-        var humidity = data.main.humidity;
-        var windSpeed = data.wind.speed;
-        var forecast = data.list;
-        // update the HTML elements
-        $("#temperature").text(temp);
-        $("#humidity").text(humidity);
-        $("#wind-speed").text(windSpeed);
-        // loop through the forecast array and display the forecast
-        for (var i = 0; i < forecast.length; i++) {
-          var day = forecast[i];
-          var date = new Date(day.dt * 1000);
-          var temp = day.main.temp;
-          $("#forecast").append("<li>" + date + " - " + temp + "</li>");
-        }
-      },
-        // parse the data and display the forecast
-  });
-}
+    success: function (data) {
+      var forecast = data.list;
+      $.each(forecast, function(index, item) {
+        var card = $("<div>").addClass("forecast-card");
+        var date = $("<p>").text(item.dt_txt);
+        var temperature = $("<p>").text(data.list[0].main.temp);
+        var windSpeed = $("<p>").text(data.list[0].wind.speed);
+        var humidity = $("<p>").text(data.list[0].main.humidity);
+        var icon = $("<img>").attr("src", "http://openweathermap.org/img/w/" + item.weather[0].icon + ".png");
+        card.append(date, temperature, windSpeed, humidity, icon);
+        $("#forecast-cards-container").append(card);
+    });
+    }
+  })
+};
